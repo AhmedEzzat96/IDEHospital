b38ce0ad702020ac4242ddfe9fbc48d5049998bb
Ahmed Ezzat <ahmed.ezzat2410@gmail.com>
2020-12-07 15:33:20 +0200

    Initial Commit

---
+++ IDEHospital/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDEHospital
+//
+//  Created by Ahmed Ezzat on 07/12/2020.
+//  Copyright © 2020 IDEAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
